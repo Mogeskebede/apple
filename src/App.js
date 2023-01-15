a207b33679303eebad9f44571a9c4c83d96b6afc
Moges Kebedew <mogeshailu381@gmail.com>
2023-01-15 15:36:55 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Component/Header";
+import Alert from "./Component/Alert";
+import Section1 from "./Component/Section1";
+import Section2 from "./Component/Section2";
+import Section3 from "./Component/Section3";
+import Section4 from "./Component/Section4";
+import Section5 from "./Component/Section5";
+import Section6 from "./Component/Section6";
+import Footer from "./Component/Footer";
+import YouTube from "./Component/YouTube";
+import News from './Component/News'
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Alert />
+			<Section1 />
+			<Section2 />
+			<Section3 />
+			<Section4 />
+			<Section5 />
+			<Section6 />
+			<YouTube />
+			<News/>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
